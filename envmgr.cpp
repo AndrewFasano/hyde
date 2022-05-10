dbac1dbd4a70f9c894103b338ae56663cfe44d17
Andrew Fasano <fasano@mit.edu>
2022-05-10 16:47:36 -0400

    EnvMgr polish

--- envmgr.cpp
+++ envmgr.cpp
@@ -1,19 +1,19 @@
 #include <asm/unistd.h> // Syscall numbers
-#include <coroutine>
-#include <stdio.h>
-#include <vector>
-#include <string>
 #include <cstring>
-#include <sys/mman.h> // for MMAP flags
+#include <stdio.h>
+#include <string>
+#include <sys/mman.h> // for mmap flags
+#include <vector>
 #include "hyde.h"
 
 bool should_coopt(void*cpu, long unsigned int callno) {
+  // We inject syscalls starting at every execve
   return callno == __NR_execve;
 }
 
 SyscCoroutine start_coopter(asid_details* details) {
-  // Variable to inject - hardcoded in here for now
-  std::string inject = "hello=world";
+  // Environment to inject - hardcoded in here for now
+  std::string inject = "HyDE_var=HyDE_val";
 
   // Get guest registers so we can examine the first argument
   struct kvm_regs regs;
@@ -22,17 +22,19 @@ SyscCoroutine start_coopter(asid_details* details) {
   std::vector<__u64> guest_arg_ptrs;
   std::vector<std::string> arg_list;
 
-  // Create `envp` - pointer to target env
-  char* fname;
-  bool success;
-  map_guest_pointer(fname, details, get_arg(regs, 0));
+  // Create guest and host envp references and use to read arguments out
   __u64 *host_envp; // Can dereference on host
-  __u64 *guest_envp = (__u64*)get_arg(regs, 2); // Can't dereference on host, just use for guest addresses
+  __u64 *guest_envp = (__u64*)get_arg(regs, 2); // Can't dereference on host, just use for addrs
   for (int i=0; i < 255; i++) {
     map_guest_pointer(host_envp, details, &guest_envp[i]);
     if (*host_envp == 0) break;
     char* env_val;
     map_guest_pointer(env_val, details, *host_envp);
+
+    if (strncmp(inject.c_str(), env_val, inject.find('=')+1) == 0) {
+      // Existing env var duplicates the one we're injecting - don't save it
+      continue;
+    }
     guest_arg_ptrs.push_back(*host_envp);
     arg_list.push_back(std::string(env_val));
   }
@@ -42,6 +44,8 @@ SyscCoroutine start_coopter(asid_details* details) {
   // injected variable. Finally, we need to change the `envp` that will be
   // used by the original execve to point to the start of our pointer list
 
+  // test with:    strace -f systemctl start snapd.service
+  // better test: /usr/lib/snapd/snapd
   __u64* guest_buf = (__u64*)yield_syscall(details, __NR_mmap,
       /*addr=*/0, /*size=*/1024, /*prot=*/PROT_READ | PROT_WRITE,
       /*flags=*/MAP_ANONYMOUS | MAP_SHARED, /*fd=*/-1, /*offset=*/0);
@@ -53,6 +57,7 @@ SyscCoroutine start_coopter(asid_details* details) {
 
   // Write our `inject` string into buffer, save it's guest addr
   char* host_buf;
+  bool success;
   map_guest_pointer_status(host_buf, details, guest_buf, &success);
   if (!success) {
     printf("FAILURE to map guest pointer at %llx\n", (__u64)guest_buf);
@@ -61,7 +66,7 @@ SyscCoroutine start_coopter(asid_details* details) {
   strncpy(host_buf, inject.c_str(), 1024);
   __u64 *injected_arg_g = guest_buf;
   // Then increment guest_buf to be right after our string
-  guest_buf += strlen(inject.c_str());
+  guest_buf += inject.length();
 
   // Now write each of the original env pointers into the buffer
   int i=0;
@@ -72,20 +77,15 @@ SyscCoroutine start_coopter(asid_details* details) {
     i++;
   }
 
-  // Add our new variable
+  // Add our new variable, then a null terminator
   map_guest_pointer(newenvp, details, &guest_buf[i]);
   *newenvp = (char*)injected_arg_g;
-  i++;
-
-  // Null terminate the new envp list
-  map_guest_pointer(newenvp, details, &guest_buf[i]);
+  map_guest_pointer(newenvp, details, &guest_buf[i+1]);
   *newenvp = (char*)0;
 
   // Finally: when HyDE goes to restore the original syscall, we *don't* want
   // to use the old arg2 (envp) value, but instead our new buffer.
-  // XXX these have to happen at the very end, otherwise subsequent injects will clobber
+  // Note these have to happen at the very end, otherwise subsequent injects will clobber
   set_ARG2(details->orig_regs, (__u64)guest_buf);
   details->modify_original_args = true;
 }
-
-
