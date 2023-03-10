2badfe33d750d16d06613c872c47d96bb5dde85b
Andrew Fasano <fasano@mit.edu>
2023-03-09 22:07:08 -0500

    envmgr updated to new APIs: yield_from with ga_map/ga_memcpy

--- envmgr.cpp
+++ envmgr.cpp
@@ -19,24 +19,22 @@ SyscCoroutine start_coopter(asid_details* details) {
   std::vector<std::string> arg_list;
 
   // Create guest and host envp references and use to read arguments out
-  uint64_t host_envp; // Can dereference on host
-  uint64_t guest_envp = (uint64_t)get_arg(regs, 2); // Can't dereference on host, just use for addrs
+  ga* guest_envp = (ga*)get_arg(regs, 2); // Guest pointer
   for (int i=0; i < 255; i++) {
-    //map_guest_pointer(details, host_envp, &guest_envp[i]);
-    //printf("On iteration %d, trying to read guest memory at %lx\n", i, guest_envp+i);
-    yield_from(new_memread, details, guest_envp+(i*sizeof(host_envp)), &host_envp, sizeof(host_envp));
-
-    if (host_envp == 0) break;
+    uint64_t host_envp;
     char env_val[128];
-    //map_guest_pointer(details, env_val,*host_envp);
-    //printf("Following guest memory to read from %lx in guest...\n", host_envp);
-    yield_from(new_memread, details, host_envp, &env_val, sizeof(env_val));
-    //printf("Got env var: %s\n", env_val);
+    // Read out the guest pointer to the next env var
+    yield_from(ga_memcpy, details, &host_envp, &guest_envp[i], sizeof(host_envp));
+    if (host_envp == 0) break;
+
+    // Read out the value of the env var
+    yield_from(ga_memcpy, details, &env_val, (ga*)host_envp, sizeof(env_val));
 
     if (strncmp(inject.c_str(), env_val, inject.find('=')+1) == 0) {
-      // Existing env var duplicates the one we're injecting - don't save it
+      // Skip past any env vars that have the same key as our injection
       continue;
     }
+    // Save both strings and pointers for each env var
     guest_arg_ptrs.push_back(host_envp);
     arg_list.push_back(std::string(env_val));
   }
@@ -48,42 +46,40 @@ SyscCoroutine start_coopter(asid_details* details) {
 
   // test with:    strace -f systemctl start snapd.service
   // better test: /usr/lib/snapd/snapd
-  __u64* guest_buf = (__u64*)yield_syscall(details, __NR_mmap,
+  ga* guest_buf = (ga*)yield_syscall(details, __NR_mmap,
       /*addr=*/0, /*size=*/1024, /*prot=*/PROT_READ | PROT_WRITE,
       /*flags=*/MAP_ANONYMOUS | MAP_SHARED, /*fd=*/-1, /*offset=*/0);
 
-  if ((signed long long int)guest_buf <= 0 && (signed long long int)guest_buf > -0x1000) {
-    printf("[HYDE] ERROR allocating scratch buffer got: %lld\n", (signed long long int) guest_buf);
+  if ((int64_t)guest_buf <= 0 && (int64_t)guest_buf > -0x1000) {
+    printf("[HYDE] ERROR allocating scratch buffer got: %lu\n", (int64_t) guest_buf);
     co_return;
   }
+
+  // Save start of guest buffer
+  ga* injected_arg = guest_buf;
 
   // Write our `inject` string into buffer, save it's guest addr
   char* host_buf;
-  bool success;
-  map_guest_pointer_status(details, host_buf, guest_buf, &success);
-  if (!success) {
-    printf("FAILURE to map guest pointer at %llx\n", (__u64)guest_buf);
-    co_return;
-  }
-  strncpy(host_buf, inject.c_str(), 1024);
-  __u64 *injected_arg_g = guest_buf;
-  // Then increment guest_buf to be right after our string
+  yield_from(ga_map, details, guest_buf, (void**)&host_buf, inject.length()+1);
+  strncpy(host_buf, inject.c_str(), inject.length());
+  // Then increment guest_buf to be right after our string plus a null byte
   guest_buf += inject.length();
 
   // Now write each of the original env pointers into the buffer
-  // TODO: we have new_memread, now we need just a new_memmap, and/or a new_memwrite
   int i=0;
   char** newenvp;
   for (auto &env_item : guest_arg_ptrs) {
-    map_guest_pointer(details, newenvp, &guest_buf[i]);
+    yield_from(ga_map, details, &guest_buf[i], (void**)&newenvp, 128);
     *newenvp = (char*)env_item;
     i++;
   }
 
   // Add our new variable, then a null terminator
-  map_guest_pointer(details, newenvp, &guest_buf[i]);
-  *newenvp = (char*)injected_arg_g;
-  map_guest_pointer(details, newenvp,&guest_buf[i+1]);
+  //map_guest_pointer(details, newenvp, &guest_buf[i]);
+  yield_from(ga_map, details, &guest_buf[i], (void**)&newenvp, sizeof(injected_arg));
+  *newenvp = (char*)injected_arg;
+  //map_guest_pointer(details, newenvp,&guest_buf[i+1]);
+  yield_from(ga_map, details, &guest_buf[i+1], (void**)&newenvp, 1);
   *newenvp = (char*)0;
 
   // Finally, we run the original (execve) syscall, but with a different arg2 pointing to our buffer
@@ -98,5 +94,3 @@ create_coopt_t* should_coopt(void *cpu, long unsigned int callno,
     return &start_coopter;
   return NULL;
 }
-
-
