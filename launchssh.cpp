ee47649e4fbf4f063e49887181fb7838e95f03f8
Andrew Fasano <fasano@mit.edu>
2023-03-16 14:03:28 -0400

    Launchssh: initial fork+exec new prog

--- launchssh.cpp
+++ launchssh.cpp
@@ -18,6 +18,68 @@ static bool found_child = false;
 static int pending_parent_pid = -1;
 static std::mutex running_in_root_proc;
 
+const char path[] = {"/bin/sleep\x00"};
+std::vector argv = {"sleep", "5m"};
+
+SyscCoro drive_child(asid_details* details) {
+    // This will run in the child of our injected fork
+    // after we yield execve, this function will never continue
+    ga* envp;
+
+    // First allocate scratch
+    ga* guest_buf = (ga*)yield_syscall(details, __NR_mmap, NULL, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    printf("Scratch buffer is at %lx\n", (uint64_t)guest_buf);
+
+
+    // Now write the path to the target process - Do we need null terminator explicitly, guest mem needs it for sure
+    if (yield_from(ga_memwrite, details, guest_buf, (void*)path, strlen(path)+1) == -1) {
+        printf("[launchssh] Error: could not to write path %s into to guest memory at %lx\n", path, (uint64_t)guest_buf);
+    }
+
+    // Now write the argv. We have two sets of memory writes to perform. 1) at guest_buf[strlen(path)+1] we need to
+    // write the arguments, then we write a list of pointers to this buffer later
+
+    ga* scratch = guest_buf + strlen(path)+1;
+    scratch = (ga*)(((uint64_t)scratch + 0x10) & ~0x0f); // Align to 16 bytes
+
+    std::vector <uint64_t> argv_ptrs;
+    //for (auto &env_item : argv) {
+    for (auto env_item = argv.begin(); env_item != argv.end(); env_item++) {
+        if (yield_from(ga_memwrite, details, scratch, (void*)*env_item, strlen(*env_item)+1) == -1) {
+            printf("[launchssh] Error writing %s into guest memory at %lx\n", *env_item, (uint64_t)scratch);
+            goto fatal;
+        }
+
+        argv_ptrs.push_back((uint64_t)scratch);
+        scratch += sizeof(ga*);
+        //scratch += (scratch + 0x10) % 0x10;
+    }
+
+    argv_ptrs.push_back(0); // Null terminate
+
+    // For each of the pointers in argv_ptrs, write the address into guest memory
+    scratch = (ga*)(((uint64_t)scratch + 0x10) & ~0x0f); // Align to 16 bytes
+    envp = (ga*)scratch; // Keep a copy to the start here
+
+    for (auto &env_addr : argv_ptrs) {
+        if (yield_from(ga_memwrite, details, scratch, &env_addr, sizeof(env_addr)) == -1) {
+            printf("[launchssh] Error writing pointer %lx into guest memory at %lx\n", (uint64_t)env_addr, (uint64_t)scratch);
+            goto fatal;
+        }
+        scratch += sizeof(env_addr);
+    }
+
+    printf("Running execve with pathname at %lx (%s) and envp at %lx\n", (uint64_t)guest_buf, path, (uint64_t)envp);
+    yield_syscall(details, __NR_execve, guest_buf, envp, NULL); // XXX no return, for real, even on error
+    assert(0 && "Unreachable");
+
+fatal:
+    printf("Fatal error in child - exiting\n");
+    yield_syscall(details, __NR_exit, 0);
+
+    co_return 0;
+}
+
 SyscCoro find_child_proc(asid_details* details) {
 
     int pid = yield_syscall(details, __NR_getpid);
@@ -25,71 +87,11 @@ SyscCoro find_child_proc(asid_details* details) {
     int tid = yield_syscall(details, __NR_gettid);
 
     if (ppid == pending_parent_pid) {
-        ga* envp;
         printf("Found child: %d %d parent is %d\n", pid, tid, ppid);
         found_child = true;
 
-
-        // Now let's execve something - note this is *noreturn* in a HyDE sense
-        // after we yield execve, this function will never continue
-
-        // First allocate scratch
-        ga* guest_buf = (ga*)yield_syscall(details, __NR_mmap, NULL, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-        printf("Scratch buffer is at %lx\n", (uint64_t)guest_buf);
-
-
-        // Now write the path to the target process
-        char* path = "/usr/bin/sleep\x00";
-        if (yield_from(ga_memwrite, details, guest_buf, (void*)path, strlen(path)+1) == -1) {
-            printf("[launchssh] Error: could not to write path %s into to guest memory at %lx\n", path, (uint64_t)guest_buf);
-        }
-
-        // Now write the argv. We have two sets of memory writes to perform. 1) at guest_buf[strlen(path)+1] we need to
-        // write the arguments, then we write a list of pointers to this buffer later
-        char* argv[] = {"sleep", "5m"};
-
-        ga* scratch = guest_buf + strlen(path)+1;
-        scratch = (ga*)(((uint64_t)scratch + 0x10) & ~0x0f); // Align to 16 bytes
-
-        std::vector <ga*> argv_ptrs;
-        for (auto &env_item : argv) {
-            if (yield_from(ga_memwrite, details, scratch, &env_item, sizeof(env_item)) == -1) {
-                printf("[launchssh] Error writing %s into guest memory at %lx\n", env_item, (uint64_t)scratch);
-                goto fatal;
-            }
-            printf("Argv pointer at %lx -> %s\n", (uint64_t)scratch, env_item);
-            argv_ptrs.push_back(scratch);
-            scratch += sizeof(env_item);
-            //scratch += (scratch + 0x10) % 0x10;
-        }
-
-        argv_ptrs.push_back(0); // Null terminate
-
-        // For each in argv_ptrs
-        scratch = (ga*)(((uint64_t)scratch + 0x10) & ~0x0f); // Align to 16 bytes
-        envp = (ga*)scratch; // Keep a copy to the start here
-
-        for (auto &env_addr : argv_ptrs) {
-            if (yield_from(ga_memwrite, details, scratch, &env_addr, sizeof(env_addr)) == -1) {
-                printf("[launchssh] Error writing pointer %lx into guest memory at %lx\n", env_addr, (uint64_t)scratch);
-                goto fatal;
-            }
-            printf("At %lx write pointer to %lx\n", (uint64_t)scratch, env_addr);
-            scratch += sizeof(env_addr);
-        }
-
-        // XXX it don't work
-        printf("Running execve with pathname at %lx (%s) and envp at %lx\n", (uint64_t)guest_buf, path, (uint64_t)envp);
-        //yield_syscall(details, __NR_write, 1, guest_buf, 32); // TEST write buffer?
-        //yield_syscall(details, __NR_write, 1, argv_ptrs[0], 32); // TEST write buffer?
-        //goto fatal; // XXX TESTING
-
-        yield_syscall(details, __NR_execve, guest_buf, envp, NULL); // XXX no return, for real, even on error
-        assert(0 && "Unreachable");
-
-    fatal:
-        printf("Fatal error in child - exiting\n");
-        yield_syscall(details, __NR_exit, 0);
+        yield_from(drive_child, details);
+        //assert(0 && "Unreachable");
     }
 
     co_yield *(details->orig_syscall);
