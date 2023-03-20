7461cdb1d03803d3de02812c048bb96cc4680c6f
Andrew Fasano <fasano@mit.edu>
2023-03-20 08:12:32 -0400

    hyperptrace updates, run server, handle some commands. needs more work still

--- hyperptrace.cpp
+++ hyperptrace.cpp
@@ -34,6 +34,43 @@ static int parent_pid = -1;
 
 static hsyscall pending_sc;
 
+char pending_command[1] = {0}; // 'g' or 'm'
+uint64_t pending_mem_addr = 0;
+uint64_t pending_mem_size = 0;
+char* pending_mem_buf = NULL;
+
+user_regs_struct gregs; // Guest registers. Populated for g command
+
+
+//  uint64_t for each of 'RAX', 'RBX', 'RCX', 'RDX', 'RSI', 'RDI', 'RBP', 'RSP', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'RIP'
+// Then uint32_t for 'EFLAGS', 'CS', 'SS', 'DS', 'ES', 'FS', 'GS'
+struct regs_struct {
+  uint64_t rax;
+  uint64_t rbx;
+  uint64_t rcx;
+  uint64_t rdx;
+  uint64_t rsi;
+  uint64_t rdi;
+  uint64_t rbp;
+  uint64_t rsp;
+  uint64_t r8;
+  uint64_t r9;
+  uint64_t r10;
+  uint64_t r11;
+  uint64_t r12;
+  uint64_t r13;
+  uint64_t r14;
+  uint64_t r15;
+  uint64_t rip;
+  uint32_t eflags;
+  uint32_t cs;
+  uint32_t ss;
+  uint32_t ds;
+  uint32_t es;
+  uint32_t fs;
+  uint32_t gs;
+};
+
 SyscCoro drive_child(asid_details* details) {
   signed long wait_rv;
 
@@ -65,34 +102,64 @@ SyscCoro drive_child(asid_details* details) {
   // "Debug loop" - either we have a command pending or we sleep (and make the debugee stall)
   // Right now we have no way to specify commands, so we just run between syscalls
 
-  int ctr = 0;
   while (true) {
-    ctr++;
-    if (ctr % 2 == 1) { // Log on return, callno + retval
-      // First get registers into guest memory
-      long int greg_rv = (long int)yield_syscall(details, __NR_ptrace, PTRACE_GETREGS, target_pid, 0, (long unsigned)guest_buf);
-      //printf("Getregs returns %ld\n", greg_rv);
+    // We have a chance to run a command in our guest process that will
+    // control the debugee. Should we do anything or just sleep?
 
-      // Read registers out of guest memory
-      user_regs_struct gregs;
+    // If we have a command, process it, then check if we have another
+    printf("Pending_command is %c\n", pending_command[0]);
+
+    if (pending_command[0] == 'g') {
+      long int greg_rv = (long int)yield_syscall(details, __NR_ptrace, PTRACE_GETREGS, target_pid, 0, (long unsigned)guest_buf);
       if (yield_from(ga_memread, details, &gregs, guest_buf, sizeof(user_regs_struct)) != 0) {
         printf("Failed to read gregs struct from guest memory\n");
         assert(0);
       }
-      printf("%2d syscall: %lld  => %llx\n", ctr/2, gregs.orig_rax, gregs.rax); // Maybe we want orig_rax?
+      pending_command[0] = (char)0;
+    } else if (pending_command[0] == 'm') {
+      //printf("Read memory from %lx, size %lx\n", pending_mem_addr, pending_mem_size);
+      // Read raw bytes into raw_buf, then convert to ascii in pending_mem_buf
+      char* raw_buf = (char*)malloc(pending_mem_size*2);
+      if (yield_from(ga_memread, details, raw_buf, (ga*)(uint64_t)pending_mem_addr, pending_mem_size) != 0) {
+        printf("Failed to read memory from guest memory\n");
+        free(raw_buf);
+        pending_mem_buf = NULL;
+      } else {
+        // Successfully read guest memory, convert it to ascii in pending_mem_buf
+        pending_mem_buf = (char*)malloc(pending_mem_size*2);
+        for (int i = 0; i < pending_mem_size; i++) {
+          sprintf(pending_mem_buf + i*2, "%02x", (unsigned char)raw_buf[i]);
+        }
+      }
+
+      // Indicate that we're done
+      pending_command[0] = (char)0;
+    } else if (pending_command[0] == (char)0) {
+      // Do a sleep for 01s
+      timespec req_h = {
+        .tv_sec = 1,
+        .tv_nsec = 0,
+      };
+
+      if (yield_from(ga_memwrite, details, guest_buf, &req_h, sizeof(timespec)) != 0) {
+        printf("Failed to write timespec to guest memory\n");
+        assert(0);
+      }
+
+      __u64 req_guest = (__u64)guest_buf;
+      __u64 rem_guest = (__u64)guest_buf + sizeof(timespec);
+      yield_syscall(details, __NR_nanosleep, guest_buf, (ga*)(uint64_t)guest_buf + sizeof(timespec));
+    } else {
+      printf("Unsupported pending command: %c. IGNORING (TODO)\n", pending_command[0]);
+      pending_command[0] = (char)0;
     }
 
-    // Run the ptrace(PTRACE_SYSCALL, target) in order to continue the target process until the next syscall
-    //printf("Continue target...\n");
-    int pt_sc_rv = yield_syscall(details, __NR_ptrace, PTRACE_SYSCALL, target_pid, 1, 0);
-    //printf("PTRACE_SYSCALL returns %d\n", pt_sc_rv);
-
+    // Resume debugee execution until it next stops XXX is this just continue?
+    #if 0
     do {
       wait_rv = yield_syscall(details, __NR_waitid, P_PID, target_pid, (long unsigned)guest_buf, WSTOPPED|WEXITED, 0);
       //printf("Waitid2 returns %ld\n", wait_rv);
     } while (wait_rv == -EINTR);
-
-    //printf("Wait RV returned %ld\n", wait_rv);
 
     if (wait_rv < 0) {
       printf("Bad wait_rv: %ld\n", wait_rv);
@@ -107,26 +174,7 @@ SyscCoro drive_child(asid_details* details) {
       printf("Debuggee exited\n");
       break;
     }
-    
-    continue;
-  #if 0
-
-    // sleep no-op
-    timespec req_h = {
-      .tv_sec = 1,
-      .tv_nsec = 0
-    };
-    // Write req_h into guest memory at guest_buf
-    if (yield_from(ga_memwrite, details, guest_buf, &req_h, sizeof(timespec)) != 0) {
-      printf("Failed to write timespec to guest memory\n");
-      assert(0);
-    }
-
-    __u64 req_guest = (__u64)guest_buf;
-    __u64 rem_guest = (__u64)guest_buf + sizeof(timespec);
-
-    yield_syscall(details, __NR_nanosleep, guest_buf, (ga*)(uint_64t)guest_buf + sizeof(timespec));
-#endif
+  #endif
   }
 
   // Finish? XXX want core platform to discard this asid? - should inejct exit
@@ -258,7 +306,7 @@ int generate_response(char* inbuffer, char*outbuffer) {
   *outp++ = '#';
   sprintf(outp, "%02x", checksum);
 
-  printf("Generated response is %s\n", outbuffer);
+  //printf("Generated response is %s\n", outbuffer);
 
   return strlen(outbuffer);
 }
@@ -306,8 +354,8 @@ int handle_message(char* buffer, size_t buffer_size, char* response) {
     }
   }
 
-  if (!has_checksum) {
-    printf("Ignoring %s since it has no checksum\n", buffer);
+  if (!has_checksum and strlen(buffer)) {
+    if (strlen(buffer) > 1) printf("Ignoring %s since it has no checksum\n", buffer);
     return 0;
   }
 
@@ -315,7 +363,7 @@ int handle_message(char* buffer, size_t buffer_size, char* response) {
   //printf("Valid packet: %s\n", packet);
 
   // Handle the packet - read the command and arguments
-  char command[256];
+  char command[256] = {0};
   char* commandp = command;
   char* argp = NULL;
   for (int i=0; i<strlen(packet); i++) {
@@ -329,80 +377,192 @@ int handle_message(char* buffer, size_t buffer_size, char* response) {
     }
   }
 
-  printf("Command: %s\n", command);
-  printf("Args: %s\n", argp);
+  printf("GOT COMMAND: %s with args %s\n", command, argp);
 
   // Single-letter commands we support
-  char * single_cmds = { "PpcsMmHXGt" };
-  char cmd[2] = { command[0], 0 };
+  //char * single_cmds = { "PpcsMmHXGt" };
+  //char cmd[2] = { command[0], 0 };
 
-  if (strstr(single_cmds, cmd) != NULL) {
-    // Cool - it's one of these single-character commands
-    switch(command[0]) {
-      case 'P':
-        // Write a register
-        int reg = strtol(&command[1], NULL, 16);
-        printf("Writing register %d\n", reg);
-        break;
-      case 'c':
-        // Continue
-        printf("Continue\n");
-        break;
-      case 's':
-        // Step
-        printf("Step %d\n", strtol(argp, NULL, 16));
-        break;
-      case 'm':
-        // Read memory
-        printf("Read memory %d\n", strtol(argp, NULL, 16));
-        break;
-      case 'M':
-        // Write memory
-        printf("Write memory %d\n", strtol(argp, NULL, 16));
-        break;
-      case 'H':
-        // Set thread
-        printf("Set thread %d\n", strtol(argp, NULL, 16));
-        break;
-      case 'X':
-        // Write memory
-        printf("Write memory %d\n", strtol(argp, NULL, 16));
-        break;
-      case 'G':
-        // Write registers
-        printf("Write registers %s %d\n", argp, strlen(argp));
-        break;
-      case 't':
-        // Is thread alive?
-        printf("Is thread alive %d\n", strtol(argp, NULL, 16));
-        break;
+  if (strlen(command) == 0) {
+    // Ignore empty packet?
+    response[0] = 0;
+    return 0;
+  }
+
+
+  switch (command[0]) {
+    case 'q': {
+      if (strcmp(command, "qSupported") == 0) {
+        snprintf(response, PACKET_SIZE, "PacketSize=%x,qXfer:exec-file:read,multiprocess+", PACKET_SIZE);
+      } else if (strcmp(command, "qTStatus") == 0) {
+        // Respond with nothing
+      } else if (strcmp(command, "qfThreadInfo") == 0) {
+        // Respond with target PID
+        sprintf(response, "%d", target_pid);
+      } else if (strcmp(command, "qAttached") == 0) {
+        // TODO: needs updating if we support multiple processes
+        sprintf(response, "%d", 0);
+
+      } else {
+        printf("TODO:  unsupported q command: %s\n", command);
+      }
+      break;
     }
-  }else {
-    // ... other commands - multi-character
+
+    case 'v':  {
+      // v things - make sure we leave vMustReplyEmpty empty and that should be the default for unsupported packets
+      if (strcmp(command, "vCont") == 0) {
+        // TODO
+      } else if (strcmp(command, "vMustReplyEmpty") == 0) {
+        // Nothing to reply with :)
+      }else {
+        printf("Unsupported v cmd: %s\n", command);
+      }
+      break;
+    }
+    case 'H':
+      // Set thread - TODO: would this allow us to change processes?
+      // For now we don't really support it
+      //printf("Set thread %d\n", strtol(argp, NULL, 16));
+        sprintf(response, "OK");
+      break;
+
+    case '?':
+      // Last signal
+      // LIE: always say it's sigstop, tell the guest the PID
+      //sprintf(response, "S05");
+      sprintf(response, "T05 swbreak;thread:%d;", target_pid);
+      break;
+
+    case 'm': {
+      // Read memory. Return as hex or indicate error with E01
+      // Extract argument: from 2nd character to coma, then to end
+
+      // Replace comma with null
+      argp  = strchr(command, ',');
+      *argp = 0;
+      argp++;
+
+      pending_mem_addr = strtol(command+1, NULL, 16);
+      pending_mem_size = strtol(argp, NULL, 16);
+
+      printf("Set M command\n");
+      pending_command[0] = 'm';
+      while (pending_command[0] != 0) {
+        // Stall until we have memory data from other thread
+        usleep(100000000); // Quick sleep
+      }
+
+      printf("Finished with M command\n");
+
+      if (pending_mem_buf == NULL) {
+        // Error - even after waiting for command, buffer is null
+        sprintf(response, "E01");
+      }else {
+        // Success
+        printf("Finished waiting for m command, now have buffer with %s", pending_mem_buf);
+        sprintf(response, "%s", pending_mem_buf);
+        printf("Pending_mem_buf: %s\n", pending_mem_buf);
+        free(pending_mem_buf);
+      }
+      break;
+    }
+
+    case 'g': {
+      // Read registers. This is the first thing that will require sync communication
+      assert(pending_command[0] == 0);
+      pending_command[0] = 'g';
+
+      while (pending_command[0] != 0) {
+        // Stall until we have register data from other thread
+        usleep(100000000);
+      }
+      printf("Finished with g command\n");
+
+      // We have data in our user_regs_struct gregs but now
+      // we need to populate a regs_struct regs with that data
+      regs_struct regs = {
+        .rax = gregs.rax,
+        .rbx = gregs.rbx,
+        .rcx = gregs.rcx,
+        .rdx = gregs.rdx,
+        .rsi = gregs.rsi,
+        .rdi = gregs.rdi,
+        .rbp = gregs.rbp,
+        .rsp = gregs.rsp,
+        .r8 = gregs.r8,
+        .r9 = gregs.r9,
+        .r10 = gregs.r10,
+        .r11 = gregs.r11,
+        .r12 = gregs.r12,
+        .r13 = gregs.r13,
+        .r14 = gregs.r14,
+        .r15 = gregs.r15,
+        .rip = gregs.rip,
+        .eflags = static_cast<uint32_t>(gregs.eflags),
+      };
+
+      // TODO that should all be big endian, I think?
+
+      // Ascii representation of regs
+      char* responsep = response;
+      for (int i=0; i<sizeof(regs_struct); i++) {
+        sprintf(responsep, "%02x", ((uint8_t*)&regs)[i]);
+        responsep += 2;
+      } 
+      break;
+    } // End of g case
+
+
+  #if 0
+    case 'P': {
+      // Write a register
+      int reg = strtol(&command[1], NULL, 16);
+      printf("Writing register %d\n", reg);
+      break;
+    }
+    case 'c':
+      // Continue
+      printf("Continue\n");
+      break;
+    case 's':
+      // Step
+      printf("Step %d\n", strtol(argp, NULL, 16));
+      break;
+    case 'M':
+      // Write memory
+      printf("Write memory %d\n", strtol(argp, NULL, 16));
+      break;
+    case 'X':
+      // Write memory
+      printf("Write memory %d\n", strtol(argp, NULL, 16));
+      break;
+    case 'G':
+      // Write registers
+      printf("Write registers %s %d\n", argp, strlen(argp));
+      break;
+    case 't':
+      // Is thread alive?
+      printf("Is thread alive %d\n", strtol(argp, NULL, 16));
+      break;
+#endif
+    default:
+      printf("Unsupported command: %s\n", command);
   }
 
-  // TODO merge with above
-  // Handle the command
-  if (strcmp(command, "qSupported") == 0) {
-    // We support the vCont command, I guess
-    sprintf(response, "PacketSize=%d;multiprocess+", PACKET_SIZE);
-  } else if (command[1] == 'v') {
-    // v things - make sure we leave vMustReplyEmpty empty and that should be the default for unsupported packets
+  //printf("Responding with %zd bytes: %s\n", strlen(response), response);
 
-  } else if (strcmp(command, "vCont") == 0) {
-    // TODO
-  }
-
-  printf("Response is %s\n", response);
-
-  return generate_response(packet, response);
+  char* scratch = strdup(response);
+  int rv= generate_response(scratch, response);
+  free(scratch);
+  return rv;
 }
 
 void handle_connections(int sockfd) {
   while (alive) {
     // Accept actual connection from the client
     int newsockfd = accept(sockfd, (struct sockaddr *) NULL, NULL);
-    if (newsockfd < 0) {k
+    if (newsockfd < 0) {
       printf("ERROR on accept %d\n", newsockfd);
       close(newsockfd);
       continue;
@@ -431,12 +591,15 @@ void handle_connections(int sockfd) {
         break;
       }
 
-      // We got a message, it's in buffer
-      char response[256] = {0};
+      // We got a message, it's in buffer. our response might be as big as an ascii
+      // buffer or all registers, but no bigger(?)
+      char response[sizeof(regs_struct)*2] = {0};
       int response_size = handle_message(buffer, n, (char*)response);
 
+      printf("Sending %d byte response %s\n", response_size, response); 
+
       // Write a response to the client
-      printf("Sending %d byte response: %s\n", response_size, response);
+      //printf("Sending %d byte response: %s\n", response_size, response);
       n = write(newsockfd, response, response_size);
       if (n < 0) {
         printf("ERROR writing to socket %d\n", n);
@@ -454,10 +617,11 @@ void __attribute__ ((constructor)) setup(void) {
     printf("Started HyperPtrace\n");
 
     // Create a listening socket and launch a thread to handle connections
-    int port = atoi(getenv("HP_PORT"));
-    if (port == 0) {
-      printf("WARN: environ var HP_PORT not set using default 1234\n");
-      port = 1234;
+    int port = 1234;
+    if (getenv("HP_PORT") != NULL && atoi(getenv("HP_PORT")) != 0) {
+      port = atoi(getenv("HP_PORT"));
+    } else {
+      printf("WARN: environ var HP_PORT not set using default %d\n", port);
     }
     int sockfd = bind_and_listen(port);
     std::thread t1(handle_connections, sockfd);
