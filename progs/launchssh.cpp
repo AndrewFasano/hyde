d736b3835eb54bf0486be61855e6fd1607533ee1
Andrew Fasano <fasano@mit.edu>
2023-03-22 14:31:53 -0400

    LaunchSSH: use new apis, helpers, and exit codes

--- progs/launchssh.cpp
+++ progs/launchssh.cpp
@@ -13,6 +13,7 @@
 #include "hyde.h"
 
 static bool done = false;
+static bool finished = false;
 static bool did_fork = false;
 static bool found_child = false;
 static int pending_parent_pid = -1;
@@ -21,47 +22,34 @@ static std::mutex running_in_root_proc;
 // What program do we run, with what args?
 const char path[] = {"/bin/systemctl"};
 std::vector argv = {"systemctl", "restart", "sshd.service"};
+#define ARGV_SIZE 3
+//const char path[] = {"/bin/sleep"};
+//std::vector argv = {"sleep", "5m"};
 
-SyscCoro drive_child(asid_details* details) {
+SyscCoroHelper drive_child(asid_details* details) {
     // This will run in the child of our injected fork
     // after we yield execve, this function will never continue
-    ga* envp;
+    uint64_t argv_addr;
 
-    // First allocate scratch
-    ga* guest_buf = (ga*)yield_syscall(details, __NR_mmap, NULL, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    printf("Scratch buffer is at %lx\n", (uint64_t)guest_buf);
+    // First allocate scratch, we'll need to persistently store our argv array in there - both values and pointers
+    uint64_t scratch = yield_syscall(details, mmap, NULL, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 
-
-    // Now write the path to the target process - Do we need null terminator explicitly, guest mem needs it for sure
-    if (yield_from(ga_memwrite, details, guest_buf, (void*)path, strlen(path)+1) == -1) {
-        printf("[launchssh] Error: could not to write path %s into to guest memory at %lx\n", path, (uint64_t)guest_buf);
-    }
-
-    // Now write the argv. We have two sets of memory writes to perform. 1) at guest_buf[strlen(path)+1] we need to
-    // write the arguments, then we write a list of pointers to this buffer later
-
-    ga* scratch = guest_buf + strlen(path)+1;
-    scratch = (ga*)(((uint64_t)scratch + 0x10) & ~0x0f); // Align to 16 bytes
-
+    // Write argv strings into guest memory, populate a vector of with these pointers
     std::vector <uint64_t> argv_ptrs;
-    //for (auto &env_item : argv) {
     for (auto env_item = argv.begin(); env_item != argv.end(); env_item++) {
+        //printf("At guest %lx write %s (size with null is %lu)\n", (uint64_t)scratch, *env_item, strlen(*env_item)+1);
         if (yield_from(ga_memwrite, details, scratch, (void*)*env_item, strlen(*env_item)+1) == -1) {
             printf("[launchssh] Error writing %s into guest memory at %lx\n", *env_item, (uint64_t)scratch);
             goto fatal;
         }
-
-        argv_ptrs.push_back((uint64_t)scratch);
-        scratch += sizeof(ga*);
-        //scratch += (scratch + 0x10) % 0x10;
+        argv_ptrs.push_back(scratch);
+        scratch += strlen(*env_item)+1;
+        scratch = ((uint64_t)scratch + 0x10) & ~0x0f; // Align to 16 bytes - probably unnecessary
     }
+    argv_ptrs.push_back(0); // And null teriminate the list of pointers
 
-    argv_ptrs.push_back(0); // Null terminate
-
-    // For each of the pointers in argv_ptrs, write the address into guest memory
-    scratch = (ga*)(((uint64_t)scratch + 0x10) & ~0x0f); // Align to 16 bytes
-    envp = (ga*)scratch; // Keep a copy to the start here
-
+    // Now write argv itself (list of pointers) into guest memory, save the start of this array into argv_addr
+    argv_addr=scratch;
     for (auto &env_addr : argv_ptrs) {
         if (yield_from(ga_memwrite, details, scratch, &env_addr, sizeof(env_addr)) == -1) {
             printf("[launchssh] Error writing pointer %lx into guest memory at %lx\n", (uint64_t)env_addr, (uint64_t)scratch);
@@ -70,52 +58,56 @@ SyscCoro drive_child(asid_details* details) {
         scratch += sizeof(env_addr);
     }
 
-    printf("Running execve with pathname at %lx (%s) and envp at %lx\n", (uint64_t)guest_buf, path, (uint64_t)envp);
-    yield_syscall(details, __NR_execve, guest_buf, envp, NULL); // XXX no return, for real, even on error
+    // We can't set an exit status directly here since we never return
+    // Instead we'll set finished, and on the next syscall we'll indicate that we're done
+    finished = true;
+    yield_syscall(details, execve, path, argv_addr, NULL); // XXX no return, for real, even on error
     assert(0 && "Unreachable");
 
 fatal:
     printf("Fatal error in child - exiting\n");
-    yield_syscall(details, __NR_exit, 0);
+    yield_syscall(details, exit, 0);
 
-    co_return 0;
+    co_return -1;
 }
 
 SyscCoro find_child_proc(asid_details* details) {
 
-    int pid = yield_syscall(details, __NR_getpid);
-    int ppid = yield_syscall(details, __NR_getppid);
-    int tid = yield_syscall(details, __NR_gettid);
+    int pid = yield_syscall0(details, getpid);
+    int ppid = yield_syscall0(details, getppid);
+    int tid = yield_syscall0(details, gettid);
 
     if (ppid == pending_parent_pid) {
         printf("Found child: %d %d parent is %d\n", pid, tid, ppid);
         found_child = true;
 
-        yield_from(drive_child, details);
-        //assert(0 && "Unreachable");
+        if (yield_from(drive_child, details) == -1) {
+            // Child process failed to run execve so we killed it. Bad but non-fatal.
+            // Set us up to retry forking another process
+            did_fork = false;
+            found_child = false;
+            co_return ExitStatus::SINGLE_FAILURE;
+        }
     }
 
     co_yield *(details->orig_syscall);
-    co_return 0;
+    co_return ExitStatus::SUCCESS;
 }
 
 SyscCoro fork_root_proc(asid_details* details) {
-    int rv = 0;
     int fd;
     int pid;
     int tid;
 
     if (done) goto out;
 
-    if (yield_syscall(details, __NR_geteuid)) {
-        rv = -1;
+    if (yield_syscall0(details, geteuid)) {
         goto out;
     }
 
     if (!running_in_root_proc.try_lock()) {
         // Lock unavailable, bail on this coopter
         // Note we don't want to wait since that would block a guest proc
-        rv = -1;
         goto out;
     }
     // Now running with the lock
@@ -123,22 +115,26 @@ SyscCoro fork_root_proc(asid_details* details) {
         goto out;
     }
 
-    pid = yield_syscall(details, __NR_getpid);
-    tid = yield_syscall(details, __NR_gettid);
+    pid = yield_syscall0(details, getpid);
+    tid = yield_syscall0(details, gettid);
     pending_parent_pid = pid;
-    printf("Fork this: %d %d\n", pid, tid);
+    //printf("Fork this: %d %d\n", pid, tid);
 
     did_fork = true;
-    yield_syscall(details, __NR_fork);
-
-    printf("Finished post-fork\n");
+    yield_syscall0(details, fork);
     done=true;
-
     running_in_root_proc.unlock();
 
 out:
     co_yield *(details->orig_syscall); // noreturn
-    co_return rv;
+    co_return ExitStatus::SUCCESS; // Even if we're waiting, it's not a failure
+}
+
+SyscCoro indicate_success(asid_details* details) {
+    // Simple coro to change nothing, but indicate that we're done
+    // This runs after we execve'd and abandoned that child
+    co_yield *(details->orig_syscall);
+    co_return ExitStatus::FINISHED;
 }
 
 create_coopt_t* should_coopt(void *cpu, long unsigned int callno,
@@ -149,6 +145,9 @@ create_coopt_t* should_coopt(void *cpu, long unsigned int callno,
 
     if (!done)
         return &fork_root_proc;
+
+    if (finished)
+        return &indicate_success;
 
   return NULL;
 }
