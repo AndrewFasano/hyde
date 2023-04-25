0673fff9593feed80d2c06d68d9a7af3ff82c506
Andrew Fasano <fasano@mit.edu>
2023-04-25 14:53:56 -0400

    Update no_root_socks

--- progs/no_root_socks.cpp
+++ progs/no_root_socks.cpp
@@ -2,38 +2,35 @@
 #include <netinet/in.h> // for sockaddr_in
 #include <arpa/inet.h> // for inet_ntoa
 #include <iostream> // for cerr
+#include "hyde_sdk.h"
 
-#include "hyde.h"
-
-SyscallCoroutine block_sock(syscall_context* details) {
-  int sockfd = get_arg(details, RegIndex::ARG0);
-  int euid = yield_syscall(details, geteuid);
+SyscallCoroutine block_sock(SyscallCtx* ctx) {
+  int sockfd = ctx->get_arg(0);
+  int euid = yield_syscall(ctx, geteuid);
 
   if (euid == 0) {
     struct sockaddr_in addr;
     socklen_t len = sizeof(addr);
 
     // get the local address associated with the socket
-    if (yield_syscall(details, getsockname, sockfd, (struct sockaddr*)&addr, &len) < 0) {
+    if (yield_syscall(ctx, getsockname, sockfd, (struct sockaddr*)&addr, &len) < 0) {
       std::cerr << "Error: getsockname failed" << std::endl;
-      co_yield *(details->orig_syscall);
+      co_yield *(ctx->get_orig_syscall());
       co_return ExitStatus::SINGLE_FAILURE;
     }
 
     // check if the local address is a loopback address
     if (addr.sin_addr.s_addr != htonl(INADDR_LOOPBACK)) {
       std::cerr << "Error: Remote listening socket detected at " << inet_ntoa(addr.sin_addr) << std::endl;
-      set_retval(details, -EADDRINUSE);
-      co_return ExitStatus::SUCCESS;
+      ctx->set_nop(-EADDRINUSE); // Instead of running syscall we'll just return this
     }
   }
 
-  co_yield *(details->orig_syscall);
+  co_yield *(ctx->get_orig_syscall());
   co_return ExitStatus::SUCCESS;
 }
 
-create_coopt_t* should_coopt(void* cpu, long unsigned int callno, long unsigned int pc, unsigned int asid) {
-  if (callno == SYS_listen)
-    return &block_sock;
-  return NULL;
+extern "C" bool init_plugin(std::unordered_map<int, create_coopter_t> map) {
+  map[SYS_listen] = block_sock;
+  return true;
 }
