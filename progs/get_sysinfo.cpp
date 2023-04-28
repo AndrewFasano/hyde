c3fa4df6127085e1b611e3a95c27bdda4e7fe762
Andrew Fasano <fasano@mit.edu>
2023-04-28 19:58:20 -0400

    Get_sysinfo: simplify and cleanup

--- progs/get_sysinfo.cpp
+++ progs/get_sysinfo.cpp
@@ -1,32 +1,20 @@
 #include <sys/sysinfo.h>
 #include "hyde_sdk.h"
 
-
-#if 0
-    // Compare to running sysinfo on the host
-    struct sysinfo host_sysinfo;
-    int host_rv = syscall(SYS_sysinfo, &host_sysinfo);
-    printf("Host sysinfo returns %d, updtime is %lu seconds\n", host_rv, host_sysinfo.uptime);
-#endif
-
-SyscallCoroutine get_sysinfo(SyscallCtx* ctx) {
-  // Example: run sysinfo in the guest
+SyscallCoroutine guest_sysinfo(SyscallCtx* ctx) {
   struct sysinfo info;
-
-  int rv = yield_syscall(ctx, sysinfo, &info);
-  if (rv != 0) {
-    co_yield_noreturn(ctx, *ctx->get_orig_syscall(), ExitStatus::SINGLE_FAILURE);
-  }
+  yield_syscall(ctx, sysinfo, &info);
   printf("Uptime is %lu seconds\n", info.uptime);
-  printf("Total RAM: %lu MB\n", info.totalram / 1024 / 1024);
-  printf("Free RAM: %lu MB\n", info.freeram / 1024 / 1024);
   printf("Number of processes: %d\n", info.procs);
-  printf("Load average (1/5/15 min): %lu %lu %lu\n", info.loads[0], info.loads[1], info.loads[2]);
+  printf("Load average (1/5/15 min): %lu %lu %lu\n",
+      info.loads[0], info.loads[1], info.loads[2]);
 
-  co_yield_noreturn(ctx, *ctx->get_orig_syscall(), ExitStatus::FINISHED);
+  // Finish by running the original syscall and unloading this plugin
+  yield_and_finish(ctx, ctx->pending_sc(), ExitStatus::FINISHED);
 }
 
-extern "C" bool init_plugin(std::unordered_map<int, create_coopter_t> map) {
-  map[-1] = get_sysinfo;
+bool init_plugin(CoopterMap map) {
+  // Before any guest process runs any syscall, launch our coroutine
+  map[-1] = guest_sysinfo;
   return true;
 }
