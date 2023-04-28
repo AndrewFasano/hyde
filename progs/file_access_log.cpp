abaef298025039b3fbeec601a67c2adbd2ffa3e7
Andrew Fasano <fasano@mit.edu>
2023-04-28 10:39:13 -0400

    File_access_log: log to files and report hyde is_paged_in stats

--- progs/file_access_log.cpp
+++ progs/file_access_log.cpp
@@ -4,64 +4,58 @@
 #include "hyde_sdk.h"
 #include "qemu_api.h"
 
-// Read the pathname passed to all open/openat syscalls.
-// Count the total number of times we see those syscalls and
-// the total number of times we can't read the pathname.
-// Report results on exit.
-
-// If BASELINE is defined, we don't use syscall injection,
-// otherwise we do.
+// Record pathname + mode passed to all open/openat syscalls
+// FOR SCIENCE: we count how often the pathname string is paged out
+// and if we're able to read it with our reliable memory access techniques
 //#define BASELINE
 
-uint64_t failc = 0;
-uint64_t goodc = 0;
+// fail/good: how many pointers could just be read
+// hydec: how many could be read with hyde
+static uint64_t total_calls = 0;
+static uint64_t native_failures = 0;
+static uint64_t hyde_failures = 0;
+
+FILE* fp;
+
 
 // Before every open check if filename is paged out or not
 SyscallCoroutine pre_open(SyscallCtx *details) {
-    // Open: path pointer is first argument
-    int path_arg = 0;
-
-    if (details->get_orig_syscall()->callno == SYS_openat) {
-        // openat: path pointer is second argument - we won't bother resolving dirfd
-        path_arg = 1;
-    }
-
+    // Called for both open and openat. If it's open path arg 0, else 1
+    int path_arg = (details->get_orig_syscall()->callno == SYS_open ? 0 : 1);
     uint64_t path_ptr = details->get_arg(path_arg);
 
-#ifdef BASELINE
+    total_calls++;
+
+    // Check if KVM can provide a translation for the path pointer and count results
     uint64_t hva;
     if (!details->translate_gva(path_ptr, &hva)) {
-        failc++;
-    } else {
-        goodc++;
+        native_failures++;
     }
-#else
-    // Read path pointer
-    char path[8];
-    if (yield_from(ga_memcpy, details, path, path_ptr, sizeof(path)) == -1) {
-        printf("Woah, we couldn't read the path pointer: %lx", path_ptr);
-    }else {
-        if (strlen(path) > 0) {
-            goodc++;
-        } else {
-            goodc++;
-            // This isn't a failure, but if we see it often, we should investigate
-            // Seems to show up once in coreutils tests suite - sounds reasonable
-            printf("\nXXX: we got an empty path string\n\n");
-        }
-    }
-#endif
 
+    // Now use HyDE APIs to do the read 
+    char path[64];
+    int bytes_read = yield_from(ga_strncpy, details, path, path_ptr, sizeof(path));
+    if (bytes_read < 0) {
+        // This should never happen - increment our counter, but also raise a fatal error
+        printf("ERROR: HyDE failed to read path pointer %lx\n", path_ptr);
+        hyde_failures++;
+        co_yield_noreturn(details, *details->get_orig_syscall(), ExitStatus::FATAL);
+    }
+
+    fprintf(fp, "File access: %s, flags %lx, mode: %lx\n", path, details->get_arg(path_arg+1), details->get_arg(path_arg+2));
     co_yield_noreturn(details, *details->get_orig_syscall(), ExitStatus::SUCCESS);
 }
 
 void __attribute__ ((destructor)) teardown(void) {
-  std::cerr << "Fail count: " << failc << std::endl;
-  std::cerr << "Success count: " << goodc << std::endl;
+    fprintf(fp, "Of the %lu open/openat syscalls, %lu could not be read with the standard approach. %lu could not be ready by hyde\n", total_calls, native_failures, hyde_failures);
+    fclose(fp);
 }
 
 extern "C" bool init_plugin(std::unordered_map<int, create_coopter_t> map) {
+  fp = fopen("opened_files.txt", "w");
+
   map[SYS_open] = pre_open;
   map[SYS_openat] = pre_open;
+
   return true;
 }
