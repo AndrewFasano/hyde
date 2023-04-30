109a9f4e75aa8627460eda20075f3ad734ae0218
Zak Estrada <zje@mit.edu>
2023-04-30 20:38:38 +0000

    untrusted_root example

--- progs/untrusted_root.cpp
+++ progs/untrusted_root.cpp
@@ -14,9 +14,6 @@
 
 #include "hyde_common.h"
 
-//Two coroutines: deny (things we never allow) and validate (things we allow)
-const char deny_msg[] = "Negative ghost rider\n";
-
 enum class DeviceType {
   CHAR,
   BLOCK
@@ -27,11 +24,16 @@ enum class DeviceType {
 const std::vector<std::tuple<int, int, DeviceType>> AllowedDevices ={
   {1, 3, DeviceType::CHAR}, // /dev/null
   {1, 5, DeviceType::CHAR}, // /dev/zero
+  {1, 7, DeviceType::CHAR}, // /dev/full
   {1, 8, DeviceType::CHAR}, // /dev/urandom
   {1, 9, DeviceType::CHAR}, // /dev/random
+  {1, 10, DeviceType::CHAR}, // /dev/aio
+  {1, 11, DeviceType::CHAR}, // /dev/kmsg
+  {1, -1, DeviceType::BLOCK}, // /dev/ramX
   {5, -1, DeviceType::CHAR}, // tty,console
-  {10, -1, DeviceType::CHAR}, // catch-all for lots of features, could get more specific
+  {10, -1, DeviceType::CHAR}, // catch-all for lots of features, could get more specific if we wanted to block some
 };
+
 //Used to Deny, but the list got long:
 //If minor is -1, we will deny all devices of that major
   /*
@@ -44,15 +46,16 @@ const std::vector<std::tuple<int, int, DeviceType>> AllowedDevices ={
   {8, -1, DeviceType::BLOCK}, // Direct Access to /dev/sd
   ...
   */
+
+#define skip_and_finish() \
+  ctx->set_nop(-1); \
+  co_yield ctx->pending_sc(); \
+  finish(ctx, ExitStatus::SUCCESS)
+
     
-  
-
-
 SyscallCoroutine deny(SyscallCtx* ctx) {
-  yield_syscall(ctx, write, 1, deny_msg, sizeof(deny_msg));
-  ctx->get_orig_syscall()->has_retval = true;
-  ctx->get_orig_syscall()->retval = -1;
-  finish(ctx, ExitStatus::SUCCESS);
+  printf("Denying syscall %lu\n", ctx->get_orig_syscall()->callno);
+  skip_and_finish();
 }
 
 SyscallCoroutine filter_open(SyscallCtx* ctx) {
@@ -74,6 +77,10 @@ SyscallCoroutine filter_open(SyscallCtx* ctx) {
     yield_and_finish(ctx, ctx->pending_sc(), ExitStatus::SINGLE_FAILURE);
   }
   if(yield_from(ga_strncpy, ctx, path, path_ptr, sizeof(path)) != -1) {
+    //Block /proc/kcore - only file that's a device that we can't stat
+    if(strcmp(path, "/proc/kcore") == 0) {
+      skip_and_finish();
+    }
     yield_syscall(ctx, statx, dirfd, path_ptr, 0, STATX_ALL, &statbuf);
     if (ctx->get_result()==0) {
       if (S_ISCHR(statbuf.stx_mode) || S_ISBLK(statbuf.stx_mode)) {
@@ -81,21 +88,58 @@ SyscallCoroutine filter_open(SyscallCtx* ctx) {
         minor = statbuf.stx_rdev_minor;
         major = statbuf.stx_rdev_major;
         std::tuple<int, int, DeviceType> searchTuple = std::make_tuple(major, minor, dev_type);
-        auto iter = std::find(AllowedDevices.begin(), AllowedDevices.end(), searchTuple);
-        printf("  statx.stx_rdev_major: %lu\n", minor);
-        printf("  statx.stx_rdev_minor: %lu\n", major);
-        if (iter == AllowedDevices.end()) {
+        bool allowed = false;
+        //Bad manual search to support wildcard
+        for (auto & element : AllowedDevices) {
+          if (std::get<0>(element) == major) {
+            if (std::get<1>(element) == minor || std::get<1>(element) == -1) {
+              if (std::get<2>(element) == dev_type) {
+                //Found it
+                allowed = true;
+                break;
+              }
+            }
+          }
+        }
+        //printf("  statx.stx_rdev_major: %lu\n", minor);
+        //printf("  statx.stx_rdev_minor: %lu\n", major);
+        if (!allowed) {
           //Not in our list
           printf("Forbidding open of device: %s\n", path);
-          deny(ctx);
+          skip_and_finish();
         }
       }
-    } else {
+    }
+    /*
+    else {
       printf("  statx failed on %s: %d\n", path, (int)ctx->get_result());
     }
+    */
   } else { 
     printf("Unable to read filename at %p\n", (void *) path_ptr);
     strcpy(path, "(error)");
+  }
+  yield_and_finish(ctx, ctx->pending_sc(), ExitStatus::SUCCESS);
+}
+
+//Check execve for LD_PRELOAD, for now just report it (could clear or block)
+SyscallCoroutine filter_execve(SyscallCtx* ctx) {
+  uint64_t envp, env_var_p;
+  char env_var_str[PATH_MAX];
+
+  for(envp = ctx->get_arg(2); envp; envp += sizeof(envp)) {
+    if(yield_from(ga_memread, ctx, &env_var_p, envp, sizeof(env_var_p)) != -1) {
+      if((void *)env_var_p == NULL) {
+        break;
+      }
+      yield_from(ga_strncpy, ctx, env_var_str, env_var_p, sizeof(env_var_str));
+      if(strncmp(env_var_str, "LD_PRELOAD=", 11) == 0) {
+        printf("LD_PRELOAD found: %s\n", env_var_str);
+      }
+    } else {
+      printf("Unable to read envp at %p\n", (void *) envp);
+      break;
+    }
   }
   yield_and_finish(ctx, ctx->pending_sc(), ExitStatus::SUCCESS);
 }
@@ -107,5 +151,6 @@ extern "C" bool init_plugin(std::unordered_map<int, create_coopter_t> map) {
   map[SYS_ptrace] = deny;
   map[SYS_open] = filter_open;
   map[SYS_openat] = filter_open;
+  map[SYS_execve] = filter_execve;
   return true;
 }
